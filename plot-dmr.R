2b577ab1090125421fa014e3e59b7418f527fd76
Brent Pedersen <bpederse@gmail.com>
2013-09-16 13:08:28 -0600

    update to use meth format

--- plot-dmr.R
+++ plot-dmr.R
@@ -1,6 +1,7 @@
 
 library(Gviz)
 library(GenomicRanges)
+library(lattice)
 source('~/src/rstuff/rmodels.R')
 options(stringsAsFactors=FALSE)
 data(cpgIslands)
@@ -10,13 +11,13 @@ args = commandArgs(TRUE)
 meth_bed = args[1]
 clin = args[2]
 id_col = args[3]
-groups = args[4]
+groups_name = args[4]
 genome = args[5]
 
 mat = read.mat(meth_bed)
 cov = read.delim(clin)
 cov$lbl = cov[,id_col]
-cov$grp = cov[,groups]
+cov$grp = cov[,groups_name]
 
 shared = intersect(cov$lbl, colnames(mat)[1:ncol(mat)])
 
@@ -26,6 +27,7 @@ locs$end = unlist(lapply(strsplit(rownames(mat), ":", fixed=TRUE), function(r) {
 
 
 mat = mat[, shared]
+gen = genome(cpgIslands)
 
 rownames(cov) = cov$lbl
 cov = cov[shared,]
@@ -34,92 +36,92 @@ groups = as.character(cov$grp)
 
 for (dmr in args[6:length(args)]){
 
-
-png_name = paste(sub(":", "-", dmr), id_col, groups, genome, "png", sep=".")
-png(png_name, width=1500, height=1200)
+    gc()
 
 
-chrom = unlist(strsplit(dmr, ":"))[1]
-se = unlist(strsplit(dmr, ":"))[2]
-dmr = unlist(lapply(strsplit(se, "-"), as.integer))
-
-gen = genome(cpgIslands)
-
-m1 = mat[(chrom == locs[,"chrom"]) & (dmr[1] < locs[,"end"]) & (dmr[2] > locs[,"start"]),]
-lm1 = locs[(chrom == locs[,"chrom"]) & (dmr[1] < locs[,"end"]) & (dmr[2] > locs[,"start"]),]
+    png_name = paste(sub(":", "-", dmr), id_col, groups_name, genome, "png", sep=".")
+    png(png_name, width=1500, height=1200)
+    message(png_name)
 
 
-dat = matrix(as.numeric(m1), nrow=nrow(m1))
-
-colnames(dat) = colnames(m1)[1:ncol(m1)]
-dat = t(dat) #[,shared])
-rownames(dat) = colnames(m1)[1:ncol(m1)]
-colnames(dat) = rownames(m1)
+    chrom = unlist(strsplit(dmr, ":"))[1]
+    se = unlist(strsplit(dmr, ":"))[2]
+    dmr = unlist(lapply(strsplit(se, "-"), as.integer))
 
 
+    m1 = mat[(chrom == locs[,"chrom"]) & (dmr[1] < locs[,"end"]) & (dmr[2] > locs[,"start"]),,drop=FALSE]
+    lm1 = locs[(chrom == locs[,"chrom"]) & (dmr[1] < locs[,"end"]) & (dmr[2] > locs[,"start"]),,drop=FALSE]
 
-cpgtrack = AnnotationTrack(cpgIslands, name="CpG", chromosome=chrom)
-gtrack = GenomeAxisTrack(range=IRanges(start=dmr[1], end=dmr[2]), fill.range="darkblue", add53=TRUE, add35=TRUE, cex=1.4)
-itrack = IdeogramTrack(genome=genome, chromosome=chrom)
+    dat = matrix(as.numeric(m1), nrow=ifelse(is.null(nrow(m1)), length(m1), nrow(m1)))
 
-
-ranges = GRanges(seqnames=rep(chrom, nrow(m1)),
-     ranges=IRanges(start=as.numeric(lm1[,"start"]), end=as.numeric(lm1[,"start"] ) + 10))
-
-
-library(lattice)
-
-colnames(dat) = paste(lm1[,1], lm1[,3], sep=":")
+    colnames(dat) = colnames(m1)[1:ncol(dat)]
+    dat = t(dat) #[,shared])
+    rownames(dat) = colnames(m1)[1:ncol(m1)]
+    colnames(dat) = rownames(m1)
 
 
-details = function(identifier, ...){
-    d = data.frame(signal=dat[,identifier], group=groups)
-    write(paste(dim(d)), stderr())
-    print(densityplot(~signal, group=group, data=d,
-             main=list(label=identifier, cex=0.7),
-                       scales=list(draw=FALSE, x=list(draw=TRUE)),
-                       ylab="", xlab="",
 
-          col=c("blue", "darkgreen"),
-          col.line=c("blue", "darkgreen"),
-          col.symbol=c("blue", "darkgreen"),
-          jitter.y=TRUE,
-          alpha=0.7,
-          cex=0.7,
-          plot.points="rug",
-                       ), newpage=FALSE,
-                      prefix="plot")
-}
-aptrackEven = AnnotationTrack(range=ranges, id=colnames(dat), fun=details, 
-                              selectFun=function(index, ...){ 
-                                  index %in% seq(2, 100, by=2)
-                              },
-                              details.size=0.96,
-                              )
-
-aptrackOdd = AnnotationTrack(range=ranges, id=colnames(dat), fun=details, 
-                              selectFun=function(index, ...){ 
-                                  index %in% seq(1, 100, by=2)
-                              },
-                              details.size=0.96,
-                              )
-
-dtrack = DataTrack(range=ranges,
-              dat=dat,
-               groups=groups,
-                genome=genome, name="methylation",
-                type=c("boxplot"),
-                col=c("blue", "darkgreen"),
-               col.frame=c(NA, NA),
-                box.width=14, legend=TRUE, cex.legend=2
-                )
+    cpgtrack = AnnotationTrack(cpgIslands, name="CpG", chromosome=chrom)
+    gtrack = GenomeAxisTrack(range=IRanges(start=dmr[1], end=dmr[2]), fill.range="darkblue", add53=TRUE, add35=TRUE, cex=1.4)
+    itrack = IdeogramTrack(genome=genome, chromosome=chrom)
 
 
-plotTracks(list(itrack, gtrack, dtrack, cpgtrack, aptrackEven, aptrackOdd),
-           from=(dmr[1] - 40), to=(dmr[2] + 40),
-           fill=c("blue", "darkgreen"),
-           background.title="darkblue", background.panel="#EFEFEF")
+    ranges = GRanges(seqnames=rep(chrom, nrow(m1)),
+         ranges=IRanges(start=as.numeric(lm1[,"start"]), end=as.numeric(lm1[,"start"] ) + 10))
 
-#warnings()
-dev.off()
+
+
+    colnames(dat) = paste(lm1[,1], lm1[,3], sep=":")
+
+
+    details = function(identifier, ...){
+        d = data.frame(signal=dat[,identifier], group=groups)
+        #write(paste(dim(d)), stderr())
+        print(densityplot(~signal, group=group, data=d,
+                 main=list(label=identifier, cex=0.7),
+                           scales=list(draw=FALSE, x=list(draw=TRUE)),
+                           ylab="", xlab="",
+
+              col=c("blue", "darkgreen"),
+              col.line=c("blue", "darkgreen"),
+              col.symbol=c("blue", "darkgreen"),
+              jitter.y=TRUE,
+              alpha=0.7,
+              cex=0.7,
+              plot.points="rug",
+                           ), newpage=FALSE,
+                          prefix="plot")
+    }
+    aptrackEven = AnnotationTrack(range=ranges, id=colnames(dat), fun=details, 
+                                  selectFun=function(index, ...){ 
+                                      index %in% seq(2, 100, by=2)
+                                  },
+                                  details.size=0.96,
+                                  )
+
+    aptrackOdd = AnnotationTrack(range=ranges, id=colnames(dat), fun=details, 
+                                  selectFun=function(index, ...){ 
+                                      index %in% seq(1, 100, by=2)
+                                  },
+                                  details.size=0.96,
+                                  )
+
+    dtrack = DataTrack(range=ranges,
+                  dat=dat,
+                   groups=groups,
+                    genome=genome, name="methylation",
+                    type=c("boxplot"),
+                    col=c("blue", "darkgreen"),
+                   col.frame=c(NA, NA),
+                    box.width=14, legend=TRUE, cex.legend=2
+                    )
+
+
+    plotTracks(list(itrack, gtrack, dtrack, cpgtrack, aptrackEven, aptrackOdd),
+               from=(dmr[1] - 40), to=(dmr[2] + 40),
+               fill=c("blue", "darkgreen"),
+               background.title="darkblue", background.panel="#EFEFEF")
+
+    #warnings()
+    dev.off()
 }
