1e968f2cd46b19d5bb71fbc04204b62fa79e7a7c
Brent Pedersen <bpederse@gmail.com>
2013-09-16 10:20:33 -0600

    make plot-dmr use the chr:pos index

--- plot-dmr.R
+++ plot-dmr.R
@@ -1,22 +1,41 @@
 
 library(Gviz)
 library(GenomicRanges)
-source('rmodels.R')
+source('~/src/rstuff/rmodels.R')
 options(stringsAsFactors=FALSE)
 data(cpgIslands)
 
 args = commandArgs(TRUE)
 
-dmr = args[1]
-meth_bed = args[2]
-clin = args[3]
-id_col = args[4]
-groups = args[5]
-genome = args[6]
+meth_bed = args[1]
+clin = args[2]
+id_col = args[3]
+groups = args[4]
+genome = args[5]
+
+mat = read.mat(meth_bed)
+cov = read.delim(clin)
+cov$lbl = cov[,id_col]
+cov$grp = cov[,groups]
+
+shared = intersect(cov$lbl, colnames(mat)[1:ncol(mat)])
+
+locs = data.frame(chrom = unlist(lapply(strsplit(rownames(mat), ":", fixed=TRUE), function(r) { r[1] })))
+locs$start = unlist(lapply(strsplit(rownames(mat), ":", fixed=TRUE), function(r) { as.numeric(r[2]) - 1 }))
+locs$end = unlist(lapply(strsplit(rownames(mat), ":", fixed=TRUE), function(r) { as.numeric(r[2]) }))
+
+
+mat = mat[, shared]
+
+rownames(cov) = cov$lbl
+cov = cov[shared,]
+
+groups = as.character(cov$grp)
+
+for (dmr in args[6:length(args)]){
 
 
 png_name = paste(sub(":", "-", dmr), id_col, groups, genome, "png", sep=".")
-print(png_name)
 png(png_name, width=1500, height=1200)
 
 
@@ -24,55 +43,33 @@ chrom = unlist(strsplit(dmr, ":"))[1]
 se = unlist(strsplit(dmr, ":"))[2]
 dmr = unlist(lapply(strsplit(se, "-"), as.integer))
 
-mat = read.mat(meth_bed)
-cov = read.delim(clin)
-cov$lbl = cov[,id_col]
-cov$grp = cov[,groups]
-
-shared = intersect(cov$lbl, colnames(mat)[3:ncol(mat)])
-mat = mat[,c("start", "end", shared)]
-print(dim(mat))
-
-rownames(cov) = cov$lbl
-cov = cov[shared,]
-print(dim(cov))
-
-groups = as.character(cov$grp)
-print(length(groups))
-print(groups)
-
 gen = genome(cpgIslands)
 
-m1 = mat[(dmr[1] < mat[,"end"]) & (dmr[2] > mat[,"start"]),]
+m1 = mat[(chrom == locs[,"chrom"]) & (dmr[1] < locs[,"end"]) & (dmr[2] > locs[,"start"]),]
+lm1 = locs[(chrom == locs[,"chrom"]) & (dmr[1] < locs[,"end"]) & (dmr[2] > locs[,"start"]),]
 
-dat = matrix(as.numeric(m1[,3:ncol(m1)]), nrow=nrow(m1))
-print("OK0")
-print(dim(dat))
-print(dim(m1))
 
-colnames(dat) = colnames(m1)[3:ncol(m1)]
+dat = matrix(as.numeric(m1), nrow=nrow(m1))
+
+colnames(dat) = colnames(m1)[1:ncol(m1)]
 dat = t(dat) #[,shared])
-print("OKa")
-rownames(dat) = colnames(m1)[3:ncol(m1)]
-print("OKb")
+rownames(dat) = colnames(m1)[1:ncol(m1)]
 colnames(dat) = rownames(m1)
 
-print("OK1")
+
 
 cpgtrack = AnnotationTrack(cpgIslands, name="CpG", chromosome=chrom)
 gtrack = GenomeAxisTrack(range=IRanges(start=dmr[1], end=dmr[2]), fill.range="darkblue", add53=TRUE, add35=TRUE, cex=1.4)
 itrack = IdeogramTrack(genome=genome, chromosome=chrom)
 
-print("OK2")
 
 ranges = GRanges(seqnames=rep(chrom, nrow(m1)),
-     ranges=IRanges(start=as.numeric(m1[,"start"]), end=as.numeric(m1[,"start"] ) + 10))
+     ranges=IRanges(start=as.numeric(lm1[,"start"]), end=as.numeric(lm1[,"start"] ) + 10))
 
 
 library(lattice)
-print("OK3")
 
-colnames(dat) = paste(m1[,1], m1[,2], sep=":")
+colnames(dat) = paste(lm1[,1], lm1[,3], sep=":")
 
 
 details = function(identifier, ...){
@@ -93,7 +90,6 @@ details = function(identifier, ...){
                        ), newpage=FALSE,
                       prefix="plot")
 }
-print("OK4")
 aptrackEven = AnnotationTrack(range=ranges, id=colnames(dat), fun=details, 
                               selectFun=function(index, ...){ 
                                   index %in% seq(2, 100, by=2)
@@ -125,3 +121,5 @@ plotTracks(list(itrack, gtrack, dtrack, cpgtrack, aptrackEven, aptrackOdd),
            background.title="darkblue", background.panel="#EFEFEF")
 
 #warnings()
+dev.off()
+}
